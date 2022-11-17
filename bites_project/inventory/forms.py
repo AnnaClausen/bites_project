75130b7501102ed6f9e95eb41cb4bc053f26f71a
Anna Clausen <102702166+AnnaClausen@users.noreply.github.com>
2022-11-17 09:38:35 -0800

    create base, home and ingredients templates and views

---
+++ bites_project/inventory/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Ingredient, MenuItem, Purchase, RecipeRequirement
+
+
+class IngredientForm(forms.ModelForm):
+    class Meta:
+        model = Ingredient
+        fields = "__all__"
+
+
+class MenuItemForm(forms.ModelForm):
+    class Meta:
+        model = MenuItem
+        fields = "__all__"
+
+
+class PurchaseForm(forms.ModelForm):
+    class Meta:
+        model = Purchase
+        fields = "__all__"
+
+class RecipeRequirementForm(forms.ModelForm):
+    class Meta:
+        model = RecipeRequirement
+        fields = "__all__"
